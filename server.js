65cbe81e74c0a9a1215ca02eaa8d365d39b27302
Vani <vaniyadavhst@gmail.com>
2026-04-01 13:24:57 +0530

    fix cors

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
